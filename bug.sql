2391fc3fd5c61a7c104456cb7cafa669a9ffae9e
BugHunter-X <askzenai@gmail.com>
2024-12-17 08:50:07 +0100

    Add bug.sql file

---
+++ bug.sql
@@ -0,0 +1,1 @@
+The SQL query uses a function that is not supported by the database system. For example, using a string function that is only available in MySQL in a PostgreSQL database.  Another instance would be using a function which exists in the database, but not in the version being used. This may manifest as a syntax error or an unexpected result.
